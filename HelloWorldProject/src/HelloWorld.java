a490f92f1b86153db082fa513fb7e0f6f0354dd8
maxjlee08 <maxjlee08@gmail.com>
2025-09-18 15:10:50 +0000

    Not finished

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * Max Lee
+ * 9/18/25
+ * 4
+ * The program will convert an inputed number into a 8 bit Binary Number
  */
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		//TODO: Implement Program Requirements Here
+		Scanner keyB = new Scanner(System.in);
+
+		System.out.println("Press 1 if you want to print a Binary to Decimal:");
+		System.out.println("Press 2 if you want to print a Decimal to Binary:");
+		int choice = keyB.nextInt();
+
+		if(choice == 1)
+		{
+			System.out.println("Input a Binary Number with 1-8 digits");
+			int binary = keyB.nextInt();
+		}
+
+		if(choice == 2)
+		{
+			System.out.println("Input a decimal number between 0-127");
+			int decimal = keyB.nextInt();
+			for(int i=0; i>= 0; i=decimal){
+				int remainder = i%2;
+				System.out.print(remainder);
+			}
+
+		}
+	
+	
+	
+	
+	
+	
 	}
 
 }
